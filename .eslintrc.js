b3d5a58ef1177dbffd298e03001eaa5f794c5a00
Mustafa <mostafa.dash34@gmail.com>
2023-04-03 20:37:32 +0200

    initialized project and added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "no-dupe-keys": "error",
+        "no-unmodified-loop-condition": 2,
+        "no-undef": 1,
+        "no-unused-vars": 1,
+        "prefer-const": 1,
+        "no-var": 2,
+        "no-multiple-empty-lines": 1,
+        "no-multi-str": 0,
+        "no-irregular-whitespace": 0,
+    }
+}
