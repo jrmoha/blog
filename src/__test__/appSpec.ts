6dc7689df6b83e6182237b3b14dc8b2844e07b8f
Mustafa <mostafa.dash34@gmail.com>
2023-05-25 21:27:34 +0300

    modified:   src/__test__/appSpec.ts

--- src/__test__/appSpec.ts
+++ src/__test__/appSpec.ts
@@ -3,10 +3,9 @@ import app from '../app';
 
 //create a request object
 const request = supertest(app);
-
 describe('Test Basic endpoint server',()=>{
-    it('Get the / endpoint' ,async () => {
-        const respons = await request.get('/');
+    it('Get the /login endpoint' ,async () => {
+        const respons = await request.get('/login');
         expect(respons.status).toBe(200);
     })
 })
